ab46ed8f50bb73c7876031235e420d470d13444a
DiegoMGonet <95249706+DiegoMGonet@users.noreply.github.com>
2022-03-10 19:44:28 -0600

    Fix | Get user Id | Diego Maldonado (#21)
    * Version 1.0
      - Change attributes for edittext events
      - Update encrypt/decrypt library for android os compatibility
      - Add create session event
      - Add register target event
      - Changes on register target event
      - Changes on picker (Spinner) event
      - Update Readme
      - Add method for set and get custom userId
      - Change the parameter type for save event
      - Update UI Test: RegisterTarget, Input, TextChange
      - fix get user Id

--- NeuroID/src/main/java/com/neuroid/tracker/storage/NIDSharedPrefsDefaults.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/storage/NIDSharedPrefsDefaults.kt
@@ -112,7 +112,7 @@ class NIDSharedPrefsDefaults(
         }
     }
 
-    fun getUserId() = sharedPref?.getString(NID_DID, "null") ?: "null"
+    fun getUserId() = sharedPref?.getString(NID_UID, "null") ?: "null"
 
     fun getDeviceId(): String {
         var deviceId = sharedPref?.getString(NID_DID, "").orEmpty()
