6c1806a82d7ea3cff1ea1212f731bb0de70d8915
DiegoMGonet <95249706+DiegoMGonet@users.noreply.github.com>
2022-04-26 12:16:59 -0500

    Fix | Sonar Cloud issues | Diego Maldonado (#27)
    * Version 1.0
      - Fix sonarcloud issues on Application
    
    Co-authored-by: alejandrobautistar <103959707+alejandrobautistar@users.noreply.github.com>

--- app/src/androidTest/java/com/sample/neuroid/us/NeuroIdUITest.kt
+++ app/src/androidTest/java/com/sample/neuroid/us/NeuroIdUITest.kt
@@ -10,8 +10,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
@@ -132,13 +131,16 @@ class NeuroIdUITest {
 
         onView(withId(R.id.button_show_activity_one_fragment))
             .perform(click())
+
         Thread.sleep(500)
 
-        onView(withId(R.id.layout_nested))
-            .perform(swipeUp())
+        onView(withId(R.id.layout_scroll)).perform(
+            swipeUp()
+        )
 
-        onView(withId(R.id.layout_nested))
-            .perform(swipeUp())
+        onView(withId(R.id.layout_scroll)).perform(
+            swipeUp()
+        )
 
         Thread.sleep(500)
 
@@ -393,7 +395,7 @@ class NeuroIdUITest {
         Thread.sleep(500) // When you go to the next test, the activity is destroyed and recreated
         onView(withId(R.id.editText_normal_field))
             .perform(click())
-        Thread.sleep(500)
+        Thread.sleep(1000)
 
         val events = getDataStoreInstance().getAllEvents()
         val event = events.firstOrNull { it.contains("\"type\":\"WINDOW_RESIZE\"") }.orEmpty()
@@ -611,7 +613,7 @@ class NeuroIdUITest {
     fun setValue(value: Int): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): org.hamcrest.Matcher<View> {
-                return ViewMatchers.isAssignableFrom(SeekBar::class.java)
+                return isAssignableFrom(SeekBar::class.java)
             }
 
             override fun getDescription(): String {
