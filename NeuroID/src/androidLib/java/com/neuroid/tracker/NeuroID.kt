0db2c7f265278096530f6f372b638041ad85b253
DiegoMGonet <95249706+DiegoMGonet@users.noreply.github.com>
2022-05-25 11:03:32 -0500

    Fix - React Native duplicated events (#48)
    * Fix duplicate events on activity callback
     * add Exclude Id method

--- NeuroID/src/androidLib/java/com/neuroid/tracker/NeuroID.kt
+++ NeuroID/src/androidLib/java/com/neuroid/tracker/NeuroID.kt
@@ -67,6 +67,12 @@ class NeuroID private constructor(
         NIDServiceTracker.screenName = screen
     }
 
+    fun excludeViewByResourceID(id: String) {
+        application?.let {
+            getDataStoreInstance().addViewIdExclude(id)
+        }
+    }
+
     fun getSessionId(): String {
         var sid = ""
         application?.let {
