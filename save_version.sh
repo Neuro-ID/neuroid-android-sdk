1a65d8a48d1ccb653e89050208b257ba21140a4f
alejandrobautista <alejandro.bautista@neuro-id.com>
2022-05-27 09:16:13 -0500

    Removing unused code

--- save_version.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-# lines that start like this are shell comments
-# read projects current directory with $PWD
-
-echo "running command from" $PWD
-cd $PWD
-git add .
-git commit -am "UPDATE new version"
-git push
