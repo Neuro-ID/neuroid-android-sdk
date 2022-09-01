6ffb35e91be449fd441500caa9d681fac07b75d3
diegomaldonado <diego.maldonado@gonet.us>
2022-09-01 11:35:32 -0500

    Task - ENG-3022 - Part 2 * Emulator validation

--- NeuroID/src/main/java/com/neuroid/tracker/utils/RootHelper.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/utils/RootHelper.kt
@@ -170,4 +170,25 @@ class RootHelper {
     }
 
     private fun checkForMagiskBinary() = checkForBinary("magisk")
+
+    fun isProbablyEmulator(): Boolean {
+        return ((Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+                && Build.FINGERPRINT.endsWith(":user/release-keys")
+                && Build.MANUFACTURER == "Google" && Build.PRODUCT.startsWith("sdk_gphone_")
+                && Build.BRAND == "google"
+                && Build.MODEL.startsWith("sdk_gphone_"))
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.BOARD == "QC_Reference_Phone" && !Build.MANUFACTURER.equals(
+            "Xiaomi",
+            ignoreCase = true
+        )
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.HOST.startsWith("Build")
+                || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                || Build.PRODUCT == "google_sdk")
+    }
 }
