3e53db4e752319925eb67b7d9615535a4e593393
DiegoMGonet <95249706+DiegoMGonet@users.noreply.github.com>
2022-09-23 10:01:09 -0500

    ENG-3246: (#102)
    * Create new methods for dynamo key

--- NeuroID/src/main/java/com/neuroid/tracker/NeuroID.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/NeuroID.kt
@@ -24,6 +24,7 @@ class NeuroID private constructor(
     private var endpoint = ENDPOINT_PRODUCTION
     private var sessionID = ""
     private var clientID = ""
+    private var timestamp: Long = 0L
 
     @Synchronized
     private fun setupCallbacks() {
@@ -101,9 +102,14 @@ class NeuroID private constructor(
         }
     }
 
+    fun getEnvironment(): String = NIDServiceTracker.environment
+
     fun setSiteId(siteId: String) {
         NIDServiceTracker.siteId = siteId
     }
+
+    fun getSiteId(): String =
+        NIDServiceTracker.siteId
 
     fun getSessionId(): String {
         return sessionID
@@ -112,6 +118,10 @@ class NeuroID private constructor(
     fun getClientId(): String {
         return clientID
     }
+
+    fun getTabId(): String = NIDServiceTracker.rndmId
+
+    fun getFirstTS(): Long = timestamp
 
     fun captureEvent(eventName: String, tgs: String) {
         application?.applicationContext?.let {
@@ -195,6 +205,7 @@ class NeuroID private constructor(
     fun isStopped() = NIDJobServiceManager.isStopped()
 
     private suspend fun createSession() {
+        timestamp = System.currentTimeMillis()
         application?.let {
             val gyroData = NIDSensorHelper.getGyroscopeInfo()
             val accelData = NIDSensorHelper.getAccelerometerInfo()
@@ -223,7 +234,7 @@ class NeuroID private constructor(
                     url = "",
                     ns = "nid",
                     jsv = NIDVersion.getSDKVersion(),
-                    ts = System.currentTimeMillis(),
+                    ts = timestamp,
                     gyro = gyroData,
                     accel = accelData
                 )
