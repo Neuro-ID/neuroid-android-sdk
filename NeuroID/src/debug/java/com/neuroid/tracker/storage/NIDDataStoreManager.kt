f464eb8e979df60947f7eb9ef76107b87a2242f1
DiegoMGonet <95249706+DiegoMGonet@users.noreply.github.com>
2023-01-13 12:16:25 -0600

    Task | ENG 4200 - Full json payload | Diego Maldonado (#137)
    * ENG-4200: Payload Json Android

---
+++ NeuroID/src/debug/java/com/neuroid/tracker/storage/NIDDataStoreManager.kt
@@ -0,0 +1,215 @@
+package com.neuroid.tracker.storage
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.neuroid.tracker.callbacks.NIDSensorHelper
+import com.neuroid.tracker.events.*
+import com.neuroid.tracker.models.NIDEventModel
+import com.neuroid.tracker.service.NIDJobServiceManager
+import com.neuroid.tracker.service.NIDServiceTracker
+import com.neuroid.tracker.utils.NIDTimerActive
+import com.neuroid.tracker.utils.NIDVersion
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
+
+interface NIDDataStoreManager {
+    fun saveEvent(event: NIDEventModel)
+    suspend fun getAllEvents(): Set<String>
+    fun addViewIdExclude(id: String)
+    suspend fun clearEvents()
+    fun resetJsonPayload()
+    fun getJsonPayload(context: Context): String
+}
+
+fun initDataStoreCtx(context: Context) {
+    NIDDataStoreManagerImp.init(context)
+}
+
+fun getDataStoreInstance(): NIDDataStoreManager {
+    return NIDDataStoreManagerImp
+}
+
+private object NIDDataStoreManagerImp : NIDDataStoreManager {
+    private const val NID_SHARED_PREF_FILE = "NID_SHARED_PREF_FILE"
+    private const val NID_STRING_EVENTS = "NID_STRING_EVENTS"
+    private const val NID_STRING_JSON_PAYLOAD = "NID_STRING_JSON_PAYLOAD"
+    private var sharedPref: SharedPreferences? = null
+    private val listNonActiveEvents = listOf(
+        USER_INACTIVE,
+        WINDOW_BLUR //Block screen
+    )
+    private val listIdsExcluded = arrayListOf<String>()
+
+    fun init(context: Context) {
+        CoroutineScope(Dispatchers.IO).launch {
+            sharedPref = context.getSharedPreferences(NID_SHARED_PREF_FILE, MODE_PRIVATE)
+        }
+    }
+
+    @Synchronized
+    override fun saveEvent(event: NIDEventModel) {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (listIdsExcluded.none { it == event.tgs || it == event.tg?.get("tgs") }) {
+                val strEvent = event.getOwnJson()
+                saveJsonPayload(strEvent, "\"${event.type}\"")
+
+                if (NIDJobServiceManager.userActive.not()) {
+                    NIDJobServiceManager.userActive = true
+                    NIDJobServiceManager.restart()
+                }
+
+                if (!listNonActiveEvents.any { strEvent.contains(it) }) {
+                    NIDTimerActive.restartTimerActive()
+                }
+                val lastEvents = getStringSet(NID_STRING_EVENTS)
+                val newEvents = LinkedHashSet<String>()
+                newEvents.addAll(lastEvents)
+                newEvents.add(strEvent)
+                putStringSet(NID_STRING_EVENTS, newEvents)
+            }
+
+            when (event.type) {
+                BLUR -> {
+                    NIDJobServiceManager.sendEventsNow()
+                }
+                CLOSE_SESSION -> {
+                    NIDJobServiceManager.sendEventsNow(true)
+                }
+            }
+        }
+    }
+
+    override suspend fun getAllEvents(): Set<String> {
+        val lastEvents = getStringSet(NID_STRING_EVENTS, emptySet())
+        clearEvents()
+
+        return lastEvents.map {
+            it.replace(
+                "\"gyro\":{\"x\":null,\"y\":null,\"z\":null}",
+                "\"gyro\":{\"x\":${NIDSensorHelper.valuesGyro.axisX},\"y\":${NIDSensorHelper.valuesGyro.axisY},\"z\":${NIDSensorHelper.valuesGyro.axisZ}}"
+            ).replace(
+                "\"accel\":{\"x\":null,\"y\":null,\"z\":null}",
+                "\"accel\":{\"x\":${NIDSensorHelper.valuesAccel.axisX},\"y\":${NIDSensorHelper.valuesAccel.axisY},\"z\":${NIDSensorHelper.valuesAccel.axisZ}}"
+            )
+        }.toSet()
+    }
+
+    override fun addViewIdExclude(id: String) {
+        if (listIdsExcluded.none { it == id }) {
+            listIdsExcluded.add(id)
+        }
+    }
+
+    override suspend fun clearEvents() {
+        putStringSet(NID_STRING_EVENTS, emptySet())
+    }
+
+    override fun resetJsonPayload() {
+        sharedPref?.let {
+            with(it.edit()) {
+                putStringSet(NID_STRING_JSON_PAYLOAD, emptySet())
+                apply()
+            }
+        }
+    }
+
+    override fun getJsonPayload(context: Context): String {
+        return createPayload(context)
+    }
+
+    private suspend fun putStringSet(key: String, stringSet: Set<String>) {
+        sharedPref?.let {
+            with(it.edit()) {
+                putStringSet(key, stringSet)
+                apply()
+            }
+        }
+    }
+
+    private suspend fun getStringSet(key: String, default: Set<String> = emptySet()): Set<String> {
+        return sharedPref?.getStringSet(key, default) ?: default
+    }
+
+    private fun saveJsonPayload(event: String, eventType: String) {
+        val jsonSet = sharedPref?.getStringSet(NID_STRING_JSON_PAYLOAD, emptySet()) ?: emptySet()
+        var shouldAdd = true
+        jsonSet.forEach {
+            if (it.contains(eventType)) {
+                shouldAdd = false
+            }
+        }
+
+        if (shouldAdd) {
+            val newEvents = LinkedHashSet<String>()
+            newEvents.addAll(jsonSet)
+            newEvents.add(event)
+
+            sharedPref?.let {
+                with(it.edit()) {
+                    putStringSet(NID_STRING_JSON_PAYLOAD, newEvents)
+                    apply()
+                }
+            }
+        }
+    }
+
+    private fun createPayload(context: Context): String {
+        val jsonSet = sharedPref?.getStringSet(NID_STRING_JSON_PAYLOAD, emptySet()) ?: emptySet()
+
+        val listEvents = jsonSet.sortedBy {
+            val event = JSONObject(it)
+            event.getLong("ts")
+        }
+
+        if (listEvents.isEmpty()) {
+            return ""
+        } else {
+            val listJson = listEvents.map {
+                if (it.contains("\"CREATE_SESSION\"")) {
+                    JSONObject(
+                        it.replace(
+                            "\"url\":\"\"",
+                            "\"url\":\"$ANDROID_URI${NIDServiceTracker.firstScreenName}\""
+                        )
+                    )
+                } else {
+                    JSONObject(it)
+                }
+            }
+
+            val jsonListEvents = JSONArray(listJson)
+
+            return getContentJson(context, jsonListEvents)
+                .replace("\\/", "/")
+        }
+    }
+
+    private fun getContentJson(
+        context: Context,
+        events: JSONArray
+    ): String {
+        val sharedDefaults = NIDSharedPrefsDefaults(context)
+
+        val jsonBody = JSONObject().apply {
+            put("siteId", NIDServiceTracker.siteId)
+            put("userId", sharedDefaults.getUserId())
+            put("clientId", sharedDefaults.getClientId())
+            put("identityId", sharedDefaults.getUserId())
+            put("pageTag", NIDServiceTracker.screenActivityName)
+            put("pageId", NIDServiceTracker.rndmId)
+            put("tabId", NIDServiceTracker.rndmId)
+            put("responseId", sharedDefaults.generateUniqueHexId())
+            put("url", "$ANDROID_URI${NIDServiceTracker.screenActivityName}")
+            put("jsVersion", "5.0.0")
+            put("sdkVersion", NIDVersion.getSDKVersion())
+            put("environment", NIDServiceTracker.environment)
+            put("jsonEvents", events)
+        }
+
+        return jsonBody.toString()
+    }
+}
---
+++ app/src/debug/java/com/sample/neuroid/us/activities/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.sample.neuroid.us.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.neuroid.tracker.NeuroID
+import com.sample.neuroid.us.R
+import com.sample.neuroid.us.activities.sandbox.SandBoxActivity
+import com.sample.neuroid.us.databinding.NidActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: NidActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        NeuroID.getInstance()?.excludeViewByResourceID("textView_sid_value")
+        binding = DataBindingUtil.setContentView(this, R.layout.nid_activity_main)
+
+        binding.apply {
+            textViewSidValue.setText(NeuroID.getInstance()?.getSessionId())
+            textViewCidValue.setText(NeuroID.getInstance()?.getClientId())
+            buttonShowActivityNoAutomaticEvents.setOnClickListener {
+                startActivity(Intent(this@MainActivity, NIDCustomEventsActivity::class.java))
+            }
+            buttonShowActivityOneFragment.setOnClickListener {
+                startActivity(Intent(this@MainActivity, NIDOnlyOneFragActivity::class.java))
+            }
+            buttonShowActivityFragments.setOnClickListener {
+                startActivity(Intent(this@MainActivity, NIDSomeFragmentsActivity::class.java))
+            }
+            buttonShowSandBox.setOnClickListener {
+                startActivity(Intent(this@MainActivity, SandBoxActivity::class.java))
+            }
+            buttonShowDynamic.setOnClickListener {
+                startActivity(Intent(this@MainActivity, DynamicActivity::class.java))
+            }
+            buttonPayloadJson.setOnClickListener {
+                startActivity(Intent(this@MainActivity, NIDPayloadJsonActivity::class.java))
+            }
+            buttonCloseSession.setOnClickListener {
+                NeuroID.getInstance()?.closeSession()
+            }
+        }
+    }
+}
