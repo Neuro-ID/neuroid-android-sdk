8b359e4de5620ca17b1805f8f2e955c6a83917f1
Clay <claytonrselby@gmail.com>
2023-01-27 11:08:02 -0700

    UserID will always be sent event if nil

--- NeuroID/src/main/java/com/neuroid/tracker/storage/NIDSharedPrefsDefaults.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/storage/NIDSharedPrefsDefaults.kt
@@ -5,6 +5,7 @@ import android.content.res.Resources
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import java.util.UUID
 import java.util.Locale
 import kotlin.random.Random
@@ -49,10 +50,10 @@ class NIDSharedPrefsDefaults(
     }
 
     // Must be set to null string
-    suspend fun getUserId(): String? {
+    suspend fun getUserId(): Any? {
         val uid = getString(NID_UID)
 
-        return uid.ifBlank { null }
+        return uid.ifBlank { JSONObject.NULL }
     }
 
     suspend fun getDeviceId(): String {
