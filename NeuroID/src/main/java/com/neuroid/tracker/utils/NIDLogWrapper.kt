565e53f97328337d2472c7ecac8ea38108b1586a
kurt ishisaka <kurtishisaka@kurts-MacBook-Pro.local>
2023-08-29 13:40:27 -0700

    ENG-5883 add log wrapper

--- NeuroID/src/main/java/com/neuroid/tracker/utils/NIDLogWrapper.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/utils/NIDLogWrapper.kt
@@ -1,18 +1,25 @@
 package com.neuroid.tracker.utils
 
+import android.util.Log
+import com.neuroid.tracker.BuildConfig
+
 class NIDLogWrapper {
     fun d(tag: String, msg: String) {
         NIDLog.d(tag, msg)
     }
+
     fun e(tag: String, msg: String) {
         NIDLog.e(tag, msg)
     }
+
     fun i(tag: String, msg: String) {
         NIDLog.i(tag, msg)
     }
+
     fun v(tag: String, msg: String) {
         NIDLog.v(tag, msg)
     }
+
     fun w(tag: String, msg: String) {
         NIDLog.w(tag, msg)
     }
