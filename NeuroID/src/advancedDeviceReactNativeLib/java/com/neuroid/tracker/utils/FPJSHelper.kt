793039ce492fc737e882b60c04462c04a8babbe5
priya-xavier <px@neuro-id.com>
2023-10-25 12:12:28 -0500

    updated imports

--- NeuroID/src/advancedDeviceReactNativeLib/java/com/neuroid/tracker/utils/FPJSHelper.kt
+++ NeuroID/src/advancedDeviceReactNativeLib/java/com/neuroid/tracker/utils/FPJSHelper.kt
@@ -1,9 +1,11 @@
 package com.neuroid.tracker.utils
+
+import android.content.Context
 import com.fingerprintjs.android.fpjs_pro.FingerprintJS
-import com.neuroid.tracker.callbacks.NIDSensorHelper
 import com.neuroid.tracker.events.ADVANCED_DEVICE_REQUEST
 import com.neuroid.tracker.events.LOG
 import com.neuroid.tracker.models.NIDEventModel
+import com.neuroid.tracker.storage.NIDSharedPrefsDefaults
 import com.neuroid.tracker.storage.getDataStoreInstance
 
 class FPJSHelper(private val context: Context) {
