ad4c7ca2e7949479539f10e4c9881424b4ef90ab
kevinsites <ks@neuro-id.com>
2023-11-27 09:58:21 -0600

    ENG-6592 - Simplify build gradle & some functions

--- NeuroID/src/main/java/com/neuroid/tracker/events/NIDIdentifyViews.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/events/NIDIdentifyViews.kt
@@ -2,57 +2,46 @@ package com.neuroid.tracker.events
 
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.RadioButton
 import android.widget.RadioGroup
-import androidx.core.view.children
+import android.widget.RatingBar
+import android.widget.SeekBar
+import android.widget.Spinner
+import android.widget.Switch
+import android.widget.ToggleButton
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.forEach
-import com.facebook.react.views.image.ReactImageView
-import com.facebook.react.views.text.ReactTextView
-import com.facebook.react.views.textinput.ReactEditText
-import com.facebook.react.views.view.ReactViewGroup
-import com.neuroid.tracker.extensions.getSHA256withSalt
 import com.neuroid.tracker.callbacks.NIDSensorHelper
-import com.neuroid.tracker.models.NIDEventModel
-import com.neuroid.tracker.service.NIDServiceTracker
 import com.neuroid.tracker.extensions.getIdOrTag
 import com.neuroid.tracker.extensions.getParents
-import org.json.JSONArray
-import org.json.JSONObject
+import com.neuroid.tracker.extensions.getSHA256withSalt
+import com.neuroid.tracker.models.NIDEventModel
+import com.neuroid.tracker.service.NIDServiceTracker
 import com.neuroid.tracker.storage.NIDDataStoreManager
 import com.neuroid.tracker.utils.NIDLogWrapper
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 fun identifyAllViews(
     viewParent: ViewGroup,
     guid: String,
     logger: NIDLogWrapper,
     storeManager: NIDDataStoreManager,
-    registerTarget: Boolean,
-    registerListeners: Boolean,
+    registerTarget: Boolean = true,
+    registerListeners: Boolean = true,
     activityOrFragment: String = "",
-    parent: String = "",
+    parent: String = ""
 ) {
     logger.d("NIDDebug identifyAllViews", "viewParent: ${viewParent.getIdOrTag()}")
 
     viewParent.forEach {
         when (it) {
             is ViewGroup -> {
-
-                if (it.hasOnClickListeners()) {
-                    val firstChild = it.children.firstOrNull()
-                    if (firstChild != null) {
-                        registerElement(
-                            it,
-                            guid,
-                            logger,
-                            storeManager,
-                            registerTarget,
-                            registerListeners,
-                            activityOrFragment = activityOrFragment,
-                            parent = parent
-                        )
-                    }
-                }
-
                 identifyAllViews(
                     it,
                     guid,
@@ -63,21 +52,24 @@ fun identifyAllViews(
                     activityOrFragment,
                     parent
                 )
-
                 it.setOnHierarchyChangeListener(object : ViewGroup.OnHierarchyChangeListener {
-
                     override fun onChildViewAdded(parent: View?, child: View?) {
                         logger.d(
                             "NIDDebug ChildViewAdded",
                             "ViewAdded: ${child?.getIdOrTag().orEmpty()}"
                         )
-
                         child?.let { view ->
+                            // This is double registering targets and registering listeners before the correct
+                            //  lifecycle event which is causing a replay of text input events to occur
+//                         identifyView(view, guid, registerTarget, registerListeners)
                         }
                     }
 
                     override fun onChildViewRemoved(parent: View?, child: View?) {
-                        logger.i("ViewListener", "ViewRemoved: ${child?.getIdOrTag().orEmpty()}")
+                        logger.d(
+                            "NIDDebug ViewListener",
+                            "ViewRemoved: ${child?.getIdOrTag().orEmpty()}"
+                        )
                     }
                 })
             }
@@ -168,6 +160,7 @@ fun identifyView(
     }
 }
 
+
 fun registerElement(
     view: View,
     guid: String,
@@ -193,22 +186,89 @@ fun registerElement(
     }
 }
 
-fun registerComponent(
+
+fun createAtrrJSON(
+    logger: NIDLogWrapper,
     view: View,
     guid: String,
+    activityOrFragment: String = "",
+    parent: String = "",
+    metaData: JSONObject
+): JSONArray {
+    val idJson = JSONObject()
+        .put("n", "guid")
+        .put("v", guid)
+
+    val classJson = JSONObject()
+        .put("n", "screenHierarchy")
+        .put("v", "${view.getParents(logger)}${NIDServiceTracker.screenName}")
+
+    val parentData =
+        JSONObject()
+            .put("parentClass", parent)
+            .put("component", activityOrFragment)
+
+    return JSONArray()
+        .put(idJson)
+        .put(classJson)
+        .put(parentData).put(metaData)
+}
+
+fun registerFinalComponent(
     logger: NIDLogWrapper,
     storeManager: NIDDataStoreManager,
     rts: String? = null,
-    activityOrFragment: String = "",
-    parent: String = "",
+    idName: String,
+    et: String,
+    v: String,
+    simpleName: String,
+    attrJson: JSONArray
 ) {
-    logger.d(
-        "NIDDebug registeredComponent",
-        "view: ${view::class} java: ${view.javaClass.simpleName}"
-    )
-
     val gyroData = NIDSensorHelper.getGyroscopeInfo()
     val accelData = NIDSensorHelper.getAccelerometerInfo()
+
+    val pathFrag = if (NIDServiceTracker.screenFragName.isEmpty()) {
+        ""
+    } else {
+        "/${NIDServiceTracker.screenFragName}"
+    }
+
+    val urlView = ANDROID_URI + NIDServiceTracker.screenActivityName + "$pathFrag/" + idName
+
+    logger.d("NID test output", "etn: INPUT, et: $simpleName, eid: $idName, v:$v")
+
+    storeManager
+        .saveEvent(
+            NIDEventModel(
+                type = REGISTER_TARGET,
+                attrs = attrJson,
+                tg = mapOf("attr" to attrJson),
+                et = "$et::$simpleName",
+                etn = "INPUT",
+                ec = NIDServiceTracker.screenName,
+                eid = idName,
+                tgs = idName,
+                en = idName,
+                v = v,
+                hv = v.getSHA256withSalt().take(8),
+                ts = System.currentTimeMillis(),
+                url = urlView,
+                gyro = gyroData,
+                accel = accelData,
+                rts = rts
+            )
+        )
+}
+
+data class ComponentValuesResult(
+    val idName: String,
+    val et: String,
+    val v: String,
+    val metaData: JSONObject
+)
+
+fun isCommonAndroidComponent(view: View): ComponentValuesResult {
+    // vars that can change based on the element
     var idName = view.getIdOrTag()
     var et = ""
     var v = "S~C~~0"
@@ -229,7 +289,7 @@ fun registerComponent(
             v = "${view.isChecked}"
 
             metaData.put("type", "radioButton")
-            metaData.put("id", "${view.getIdOrTag()}")
+            metaData.put("id", "$idName")
 
             // go up to 3 parents in case a RadioGroup is not the direct parent
             var rParent = view.parent;
@@ -247,18 +307,17 @@ fun registerComponent(
         is RadioGroup -> {
             et = "RadioGroup"
             v = "${view.checkedRadioButtonId}"
-
         }
 
         is ToggleButton -> {
             et = "ToggleButton"
         }
 
-        is Switch -> {
+        is Switch, is SwitchCompat -> {
             et = "Switch"
         }
 
-        is Button -> {
+        is Button, is ImageButton -> {
             et = "Button"
         }
 
@@ -270,72 +329,15 @@ fun registerComponent(
             et = "Spinner"
         }
 
-        is ReactEditText -> {
-            et = "ReactEditText"
-            v = "S~C~~${view.text?.length}"
-        }
-
-        is ReactViewGroup -> {
-            if (view.hasOnClickListeners() && view.children.count() == 1) {
-                val child = view.children.firstOrNull()
-                if (child is ReactTextView) {
-                    et = "ReactButton::${child.javaClass.simpleName}"
-                    idName = "${idName}-${
-                        child?.getIdOrTag()
-                    }-${child?.text.toString().getSHA256withSalt()?.take(8)}"
-
-                } else if (child is ReactImageView) {
-                    et = "ReactButton::${child.javaClass.simpleName}"
-                       idName = "${idName}-${
-                        child?.getIdOrTag()
-                    }"
-                }
-            }
+        is RatingBar -> {
+            et = "RatingBar"
         }
     }
 
-
-    logger.d("NIDDebug et at registerComponent", "${et}")
-
-    // early exit if not supported target type
-    if (et.isEmpty()) {
-        return
-    }
-
-    val pathFrag = if (NIDServiceTracker.screenFragName.isEmpty()) {
-        ""
-    } else {
-        "/${NIDServiceTracker.screenFragName}"
-    }
-    val urlView = ANDROID_URI + NIDServiceTracker.screenActivityName + "$pathFrag/" + idName
-
-    val idJson = JSONObject().put("n", "guid").put("v", guid)
-    val classJson = JSONObject().put("n", "screenHierarchy")
-        .put("v", "${view.getParents(logger)}${NIDServiceTracker.screenName}")
-    val parentData =
-        JSONObject().put("parentClass", "$parent").put("component", "$activityOrFragment")
-
-    val attrJson = JSONArray().put(idJson).put(classJson).put(parentData).put(metaData)
-
-    storeManager
-        .saveEvent(
-            NIDEventModel(
-                type = REGISTER_TARGET,
-                attrs = attrJson,
-                tg = mapOf("attr" to attrJson),
-                et = et + "::" + view.javaClass.simpleName,
-                etn = "INPUT",
-                ec = NIDServiceTracker.screenName,
-                eid = idName,
-                tgs = idName,
-                en = idName,
-                v = v,
-                hv = v.getSHA256withSalt()?.take(8),
-                ts = System.currentTimeMillis(),
-                url = urlView,
-                gyro = gyroData,
-                accel = accelData,
-                rts = rts
-            )
-        )
+    return ComponentValuesResult(
+        idName,
+        et,
+        v,
+        metaData
+    )
 }
