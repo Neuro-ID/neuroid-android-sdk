1404cbe28c268e3e6d369fef63ce75ce287207e1
kevinsites <ks@neuro-id.com>
2024-02-13 11:17:26 -0700

    ENG-6166 - NIDEventModel Tests

---
+++ NeuroID/src/test/java/com/neuroid/tracker/NIDEventModelUnitTest.kt
@@ -0,0 +1,39 @@
+package com.neuroid.tracker
+
+import android.content.res.Resources
+import android.view.View
+import com.neuroid.tracker.extensions.getIdOrTag
+import com.neuroid.tracker.models.NIDEventModel
+import io.mockk.every
+import io.mockk.mockk
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NIDEventModelUnitTest {
+    @Test
+    fun testToJSONSimple() {
+        val event = NIDEventModel(type = "TEST_TYPE", ts = 1)
+        val result = event.getOwnJson()
+
+        assertEquals("{\"type\":\"TEST_TYPE\",\"ts\":1}", result)
+    }
+
+    @Test
+    fun testToJSONAdvanced() {
+        val event = NIDEventModel(
+            type = "TEST_TYPE",
+            ts = 1,
+            attrs = JSONArray().put(
+                        JSONObject().put("key", "value")
+                    ),
+            tg = mapOf("key1" to "value1", "key2" to "value2"),
+            dnt = false
+            )
+        val result = event.getOwnJson()
+
+        assertEquals("{\"tg\":{\"key1\":\"value1\",\"key2\":\"value2\"},\"dnt\":false,\"type\":\"TEST_TYPE\",\"attrs\":[{\"key\":\"value\"}],\"ts\":1}", result)
+    }
+}
