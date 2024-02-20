fefe11549cff1de4fff4dbc8c8c10c37ef95184b
kevinsites <ks@neuro-id.com>
2024-02-20 13:18:50 -0700

    Fix dumb RN error

--- NeuroID/src/test/java/com/neuroid/tracker/NIDDataStoreManagerUnitTests.kt
+++ NeuroID/src/test/java/com/neuroid/tracker/NIDDataStoreManagerUnitTests.kt
@@ -174,25 +174,4 @@ class NIDDataStoreManagerUnitTests {
         val events = dataStore.getAllEvents()
         Assert.assertEquals(0, events.count())
     }
-
-    @Test
-    fun testPayload(){
-
-
-       val payload =  NIDServiceTracker.getContentJson(
-            mockContext,
-            listOf(
-                NIDEventModel(
-                    type = "TEST",
-                    tg = hashMapOf(
-                        "attr" to JsonUtils.getAttrJson("my string"),
-                    ),
-                    ts = 1,
-                )
-            )
-        )
-
-        println(payload)
-        assert(false)
-    }
 }
