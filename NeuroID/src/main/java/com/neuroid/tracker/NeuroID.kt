71859a294964be40a80156a6c3a15d6d8e2dbe0c
kevinsites <ks@neuro-id.com>
2024-03-11 10:33:56 -0600

    ENG-7454 - Return value on stop

--- NeuroID/src/main/java/com/neuroid/tracker/NeuroID.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/NeuroID.kt
@@ -438,10 +438,12 @@ private constructor(
         return true
     }
 
-    fun stop() {
-        pauseCollection()
-        nidCallActivityListener?.unregisterCallActivityListener(this.getApplicationContext())
-    }
+        fun stop(): Boolean {
+            pauseCollection()
+            nidCallActivityListener?.unregisterCallActivityListener(this.getApplicationContext())
+
+            return true
+        }
 
     fun closeSession() {
         if (!isStopped()) {
