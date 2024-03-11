881cc83308131af32a324af94c9e07c239428d4a
kevinsites <ks@neuro-id.com>
2024-03-11 11:21:36 -0600

    Fix merge conflicts

--- NeuroID/src/main/java/com/neuroid/tracker/callbacks/NIDContextMenuCallbacks.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/callbacks/NIDContextMenuCallbacks.kt
@@ -8,24 +8,25 @@ import com.neuroid.tracker.events.CONTEXT_MENU
 import com.neuroid.tracker.events.COPY
 import com.neuroid.tracker.events.CUT
 import com.neuroid.tracker.events.PASTE
+import com.neuroid.tracker.utils.NIDLogWrapper
 
 abstract class NIDContextMenuCallBacks(
-        val neuroID: NeuroID,
-        actionCallBack: ActionMode.Callback?,
+    val neuroID: NeuroID,
+    actionCallBack: ActionMode.Callback?,
 ) : ActionMode.Callback {
     val wrapper = actionCallBack
 
     override fun onCreateActionMode(
-            action: ActionMode?,
-            menu: Menu?,
+        action: ActionMode?,
+        menu: Menu?,
     ): Boolean {
         wrapper?.onCreateActionMode(action, menu)
         return true
     }
 
     override fun onPrepareActionMode(
-            action: ActionMode?,
-            menu: Menu?,
+        action: ActionMode?,
+        menu: Menu?,
     ): Boolean {
         wrapper?.onPrepareActionMode(action, menu)
         return false
@@ -36,28 +37,28 @@ abstract class NIDContextMenuCallBacks(
     }
 
     internal fun saveEvent(
-            option: Int,
-            item: String,
+        option: Int,
+        item: String,
     ) {
         val type =
-                when (option) {
-                    android.R.id.paste -> PASTE
-                    android.R.id.copy -> COPY
-                    android.R.id.cut -> CUT
-                    else -> ""
-                }
+            when (option) {
+                android.R.id.paste -> PASTE
+                android.R.id.copy -> COPY
+                android.R.id.cut -> CUT
+                else -> ""
+            }
 
         if (type.isNotEmpty()) {
             neuroID.captureEvent(
-                    type = CONTEXT_MENU,
-                    attrs =
-                            listOf(
-                                    mapOf(
-                                            "option" to "$option",
-                                            "item" to item,
-                                            "type" to type,
-                                    ),
-                            ),
+                type = CONTEXT_MENU,
+                attrs =
+                    listOf(
+                        mapOf(
+                            "option" to "$option",
+                            "item" to item,
+                            "type" to type,
+                        ),
+                    ),
             )
         }
     }
@@ -65,13 +66,13 @@ abstract class NIDContextMenuCallBacks(
 
 // This is the callback for the context menu that appears when text is already in field
 class NIDTextContextMenuCallbacks(
-        neuroID: NeuroID,
-        val logger: NIDLogWrapper,
-        actionCallBack: ActionMode.Callback?
+    neuroID: NeuroID,
+    val logger: NIDLogWrapper,
+    actionCallBack: ActionMode.Callback?,
 ) : NIDContextMenuCallBacks(neuroID, actionCallBack) {
     override fun onActionItemClicked(
-            action: ActionMode?,
-            item: MenuItem?,
+        action: ActionMode?,
+        item: MenuItem?,
     ): Boolean {
         wrapper?.onActionItemClicked(action, item)
 
@@ -85,13 +86,13 @@ class NIDTextContextMenuCallbacks(
 // This is the callback for the context menu that appears when the text field is empty (only
 // available in later API versions)
 class NIDLongPressContextMenuCallbacks(
-        neuroID: NeuroID,
-        val logger: NIDLogWrapper,
-        actionCallBack: ActionMode.Callback?
+    neuroID: NeuroID,
+    val logger: NIDLogWrapper,
+    actionCallBack: ActionMode.Callback?,
 ) : NIDContextMenuCallBacks(neuroID, actionCallBack) {
     override fun onActionItemClicked(
-            action: ActionMode?,
-            item: MenuItem?,
+        action: ActionMode?,
+        item: MenuItem?,
     ): Boolean {
         wrapper?.onActionItemClicked(action, item)
 
