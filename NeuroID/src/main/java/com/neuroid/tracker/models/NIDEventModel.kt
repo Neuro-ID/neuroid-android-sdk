729f130a3f6373b2bccbdba918b021f7b26e2d01
kevinsites <102255450+kevinsites@users.noreply.github.com>
2024-07-19 13:18:14 -0600

    ENG-8348 - Call In Progress Update (#344)
    Co-authored-by: priya-xavier <98341103+priya-xavier@users.noreply.github.com>

--- NeuroID/src/main/java/com/neuroid/tracker/models/NIDEventModel.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/models/NIDEventModel.kt
@@ -5,6 +5,7 @@ import com.neuroid.tracker.events.ADVANCED_DEVICE_REQUEST
 import com.neuroid.tracker.events.APPLICATION_SUBMIT
 import com.neuroid.tracker.events.ATTEMPTED_LOGIN
 import com.neuroid.tracker.events.BLUR
+import com.neuroid.tracker.events.CALL_IN_PROGRESS
 import com.neuroid.tracker.events.CLOSE_SESSION
 import com.neuroid.tracker.events.CONTEXT_MENU
 import com.neuroid.tracker.events.COPY
@@ -233,6 +234,7 @@ data class NIDEventModel(
                 LOG -> contextString = "m=${this.m}, ts=${this.ts}, level=${this.level}"
                 NETWORK_STATE -> contextString = "iswifi=${this.isWifi}, isconnected=${this.isConnected}"
                 ATTEMPTED_LOGIN -> contextString = "uid=${this.uid}"
+                CALL_IN_PROGRESS -> contextString = "cp=${this.cp}, metadata=${this.attrs}"
                 else -> {}
             }
 
