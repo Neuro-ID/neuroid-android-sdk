9c776ec28fe8282c933c7062ce8b278034f74f0f
Kevin Sites <102255450+kevinsites@users.noreply.github.com>
2024-08-03 10:41:03 -0600

    ENG-8435 - Capture Compose Button Tap (#355)

--- NeuroID/src/main/java/com/neuroid/tracker/NeuroID.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/NeuroID.kt
@@ -883,6 +883,7 @@ class NeuroID
             isConnected: Boolean? = null,
             cp: String? = null,
             l: Long? = null,
+            synthetic: Boolean? = null,
         ) {
             if (!queuedEvent && (!isSDKStarted || nidJobServiceManager?.isStopped() == true)) {
                 return
@@ -962,6 +963,7 @@ class NeuroID
                     isConnected,
                     cp,
                     l,
+                    synthetic,
                 )
 
             if (queuedEvent) {
--- NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
@@ -1,6 +1,8 @@
 package com.neuroid.tracker.compose
 
 import com.neuroid.tracker.NeuroID
+import com.neuroid.tracker.events.TOUCH_END
+import com.neuroid.tracker.events.TOUCH_START
 import com.neuroid.tracker.utils.NIDLogWrapper
 
 interface JetpackCompose {
@@ -34,7 +36,34 @@ class JetpackComposeImpl(
         elementName: String,
         pageName: String,
     ) {
-        TODO("Not yet implemented - Placeholder")
+        val sdkMap =
+            mapOf(
+                "jetpackCompose" to true,
+            )
+
+        neuroID.captureEvent(
+            type = TOUCH_START,
+            ec = pageName,
+            tgs = elementName,
+            tg = sdkMap,
+            attrs =
+                listOf(
+                    sdkMap,
+                ),
+            synthetic = true,
+        )
+
+        neuroID.captureEvent(
+            type = TOUCH_END,
+            ec = pageName,
+            tgs = elementName,
+            tg = sdkMap,
+            attrs =
+                listOf(
+                    sdkMap,
+                ),
+            synthetic = true,
+        )
     }
 
     override fun trackPage(pageName: String) {
