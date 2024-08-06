aae80a4a2b074a3cf9418441a0e9ec478f4fa8e1
priya-xavier <98341103+priya-xavier@users.noreply.github.com>
2024-08-06 13:51:14 -0400

    ENG-8436-window load composable events (#356)

--- NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
@@ -1,8 +1,12 @@
 package com.neuroid.tracker.compose
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import com.neuroid.tracker.NeuroID
 import com.neuroid.tracker.events.TOUCH_END
 import com.neuroid.tracker.events.TOUCH_START
+import com.neuroid.tracker.events.WINDOW_LOAD
+import com.neuroid.tracker.events.WINDOW_UNLOAD
 import com.neuroid.tracker.utils.NIDLogWrapper
 
 interface JetpackCompose {
@@ -17,12 +21,14 @@ interface JetpackCompose {
         pageName: String,
     )
 
+    @Composable
     fun trackPage(pageName: String)
 }
 
 class JetpackComposeImpl(
-    val neuroID: NeuroID,
-    val logger: NIDLogWrapper,
+    val neuroID: NeuroID, val logger: NIDLogWrapper, val sdkMap: Map<String, Boolean> = mapOf(
+        "jetpackCompose" to true,
+    )
 ) : JetpackCompose {
     override fun trackElement(
         elementState: String,
@@ -36,20 +42,14 @@ class JetpackComposeImpl(
         elementName: String,
         pageName: String,
     ) {
-        val sdkMap =
-            mapOf(
-                "jetpackCompose" to true,
-            )
-
         neuroID.captureEvent(
             type = TOUCH_START,
             ec = pageName,
             tgs = elementName,
             tg = sdkMap,
-            attrs =
-                listOf(
-                    sdkMap,
-                ),
+            attrs = listOf(
+                sdkMap,
+            ),
             synthetic = true,
         )
 
@@ -58,15 +58,28 @@ class JetpackComposeImpl(
             ec = pageName,
             tgs = elementName,
             tg = sdkMap,
-            attrs =
-                listOf(
-                    sdkMap,
-                ),
+            attrs = listOf(
+                sdkMap,
+            ),
             synthetic = true,
         )
     }
 
+    @Composable
     override fun trackPage(pageName: String) {
-        TODO("Not yet implemented - Placeholder")
+        DisposableEffect(Unit) {
+            captureComposeWindowEvent(pageName = pageName, WINDOW_LOAD)
+            onDispose {
+                captureComposeWindowEvent(pageName = pageName, WINDOW_UNLOAD)
+            }
+        }
+    }
+
+    internal fun captureComposeWindowEvent(pageName: String, type: String) {
+        neuroID.captureEvent(
+            type = type, ec = pageName, attrs = listOf(
+                sdkMap
+            )
+        )
     }
 }
