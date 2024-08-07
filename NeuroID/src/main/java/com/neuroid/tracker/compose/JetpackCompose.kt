a9d352a562e0b0e1536b2b1ca1c407cdd32e66d2
kishisaka-nid <139158682+kishisaka-nid@users.noreply.github.com>
2024-08-07 08:55:47 -0700

    ENG-8434 compose text watcher (#357)

--- NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
@@ -1,8 +1,18 @@
 package com.neuroid.tracker.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.runtime.DisposableEffect
 import com.neuroid.tracker.NeuroID
+import com.neuroid.tracker.events.INPUT
+import com.neuroid.tracker.events.PASTE
+import com.neuroid.tracker.extensions.getSHA256withSalt
+import com.neuroid.tracker.utils.JsonUtils.Companion.getAttrJson
 import com.neuroid.tracker.events.TOUCH_END
 import com.neuroid.tracker.events.TOUCH_START
 import com.neuroid.tracker.events.WINDOW_LOAD
@@ -10,6 +20,7 @@ import com.neuroid.tracker.events.WINDOW_UNLOAD
 import com.neuroid.tracker.utils.NIDLogWrapper
 
 interface JetpackCompose {
+    @Composable
     fun trackElement(
         elementState: String,
         elementName: String,
@@ -30,14 +41,6 @@ class JetpackComposeImpl(
         "jetpackCompose" to true,
     )
 ) : JetpackCompose {
-    override fun trackElement(
-        elementState: String,
-        elementName: String,
-        pageName: String,
-    ) {
-        TODO("Not yet implemented - Placeholder")
-    }
-
     override fun trackButtonTap(
         elementName: String,
         pageName: String,
@@ -81,5 +84,63 @@ class JetpackComposeImpl(
                 sdkMap
             )
         )
+    }
+
+    @Composable
+    override fun trackElement(
+        elementState: String,
+        elementName: String,
+        pageName: String,
+    ) {
+        val clipboardManager = LocalClipboardManager.current
+        // save instance of previous text and previous pasted text hashcode
+        var previousText by remember { mutableStateOf(elementState) }
+        var lastPastedHashCode by remember { mutableStateOf(0) }
+
+        // handle the text changes
+        LaunchedEffect(elementState) {
+            if (!neuroID.isStopped() && elementState != previousText) {
+                val changeText = neuroID.nidComposeTextWatcher.getTextChange(previousText, elementState)
+                val clipboardContent = clipboardManager.getText()?.text ?: ""
+                // check for paste, send paste event if so
+                if (neuroID.nidComposeTextWatcher.isPaste(changeText, clipboardContent, lastPastedHashCode)) {
+                    neuroID.captureEvent(
+                        type = PASTE,
+                        tg =
+                            hashMapOf(
+                                "attr" to getAttrJson(elementState),
+                                "et" to "text",
+                            ),
+                        tgs = "$pageName:$elementName",
+                        v = "S~C~~${clipboardContent.length}",
+                        hv = clipboardContent.getSHA256withSalt().take(8),
+                        attrs =
+                            listOf(
+                                mapOf(
+                                    "clipboardText" to "S~C~~${clipboardContent.length}",
+                                ),
+                                sdkMap
+                            ),
+                    )
+                    lastPastedHashCode = clipboardContent.hashCode()
+                }
+                neuroID.captureEvent(
+                    type = INPUT,
+                    tg =
+                        hashMapOf(
+                            "attr" to getAttrJson(elementState),
+                            "etn" to INPUT,
+                            "et" to "text",
+                        ),
+                    tgs = "$pageName:$elementName",
+                    v = "S~C~~${elementState.length}",
+                    hv = elementState.getSHA256withSalt().take(8),
+                    attrs = listOf(
+                       sdkMap
+                    )
+                )
+                previousText = elementState
+            }
+        }
     }
 }
