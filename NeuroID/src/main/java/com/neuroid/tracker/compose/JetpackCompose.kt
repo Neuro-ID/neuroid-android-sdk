262d2f5866ef9becf9fadc42501d95d534bf411f
kevinsites <ks@neuro-id.com>
2024-08-15 09:15:18 -0600

    ENG-8489 - Add New method to track page and elements in one, add secondary track elements method, add register target event

--- NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/compose/JetpackCompose.kt
@@ -2,28 +2,36 @@ package com.neuroid.tracker.compose
 
 import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.runtime.DisposableEffect
 import com.neuroid.tracker.NeuroID
 import com.neuroid.tracker.events.INPUT
 import com.neuroid.tracker.events.PASTE
-import com.neuroid.tracker.extensions.getSHA256withSalt
-import com.neuroid.tracker.utils.JsonUtils.Companion.getAttrJson
+import com.neuroid.tracker.events.REGISTER_TARGET
 import com.neuroid.tracker.events.TOUCH_END
 import com.neuroid.tracker.events.TOUCH_START
 import com.neuroid.tracker.events.WINDOW_LOAD
 import com.neuroid.tracker.events.WINDOW_UNLOAD
+import com.neuroid.tracker.extensions.getSHA256withSalt
+import com.neuroid.tracker.utils.JsonUtils.Companion.getAttrJson
 import com.neuroid.tracker.utils.NIDLogWrapper
 
 interface JetpackCompose {
     @Composable
     fun trackElement(
         elementState: String,
+        elementName: String,
+        pageName: String,
+    )
+
+    fun trackElementOnChange(
+        oldState: String,
+        newState: String,
         elementName: String,
         pageName: String,
     )
@@ -35,19 +43,31 @@ interface JetpackCompose {
 
     @Composable
     fun trackPage(pageName: String)
+
+    @Composable
+    fun trackPageElements(
+        pageName: String,
+        elements: Array<Pair<String, String>>,
+    )
 }
 
 class JetpackComposeImpl(
-    val neuroID: NeuroID, val logger: NIDLogWrapper, val sdkMap: Map<String, Boolean> = mapOf(
-        "jetpackCompose" to true,
-    )
+    val neuroID: NeuroID,
+    val logger: NIDLogWrapper,
+    val sdkMap: Map<String, Boolean> =
+        mapOf(
+            "jetpackCompose" to true,
+        ),
 ) : JetpackCompose {
+    /**
+     * Track a compose button onClick
+     */
     override fun trackButtonTap(
         elementName: String,
         pageName: String,
     ) {
         if (elementName.isEmpty() || pageName.isEmpty()) {
-            Log.d("NeuroID Debug Event","Skipping button tracking since either name: ${elementName} or ${pageName} is empty")
+            Log.d("NeuroID Debug Event", "Skipping button tracking since either name: $elementName or $pageName is empty")
             return
         }
 
@@ -56,9 +76,10 @@ class JetpackComposeImpl(
             ec = pageName,
             tgs = elementName,
             tg = sdkMap,
-            attrs = listOf(
-                sdkMap,
-            ),
+            attrs =
+                listOf(
+                    sdkMap,
+                ),
             synthetic = true,
         )
 
@@ -67,13 +88,17 @@ class JetpackComposeImpl(
             ec = pageName,
             tgs = elementName,
             tg = sdkMap,
-            attrs = listOf(
-                sdkMap,
-            ),
+            attrs =
+                listOf(
+                    sdkMap,
+                ),
             synthetic = true,
         )
     }
 
+    /**
+     * Track a compose page load/unload
+     */
     @Composable
     override fun trackPage(pageName: String) {
         DisposableEffect(Unit) {
@@ -84,14 +109,38 @@ class JetpackComposeImpl(
         }
     }
 
-    internal fun captureComposeWindowEvent(pageName: String, type: String) {
+    internal fun captureComposeWindowEvent(
+        pageName: String,
+        type: String,
+    ) {
         neuroID.captureEvent(
-            type = type, ec = pageName, attrs = listOf(
-                sdkMap
-            )
+            type = type,
+            ec = pageName,
+            attrs =
+                listOf(
+                    sdkMap,
+                ),
         )
     }
 
+    /**
+     * Track a compose page and the associated elements on the page
+     */
+    @Composable
+    override fun trackPageElements(
+        pageName: String,
+        elements: Array<Pair<String, String>>,
+    ) {
+        trackPage(pageName = pageName)
+
+        elements.forEach { pair ->
+            trackElement(elementState = pair.second, elementName = pair.first, pageName = pageName)
+        }
+    }
+
+    /**
+     * Track a compose element on the page and its state changes through LaunchedEffect listeners
+     */
     @Composable
     override fun trackElement(
         elementState: String,
@@ -103,7 +152,27 @@ class JetpackComposeImpl(
         var previousText by remember { mutableStateOf(elementState) }
         var lastPastedHashCode by remember { mutableStateOf(0) }
 
-        // handle the text changes
+        // Runs only on init launch so we can capture the target registration
+        LaunchedEffect(Unit) {
+            neuroID.captureEvent(
+                type = REGISTER_TARGET,
+                ec = pageName,
+                eid = elementName,
+                tgs = elementName,
+                en = elementName,
+                v = "S~C~~${elementState.length}",
+                hv = elementState.getSHA256withSalt().take(8),
+                rts = "true",
+                attrs =
+                    listOf(
+                        sdkMap,
+                    ),
+                etn = INPUT,
+                et = "Edittext",
+            )
+        }
+
+        // runs whenever the element state changes (i.e. text changes)
         LaunchedEffect(elementState) {
             if (!neuroID.isStopped() && elementState != previousText) {
                 val changeText = neuroID.nidComposeTextWatcher.getTextChange(previousText, elementState)
@@ -126,7 +195,7 @@ class JetpackComposeImpl(
                                 mapOf(
                                     "clipboardText" to "S~C~~${clipboardContent.length}",
                                 ),
-                                sdkMap
+                                sdkMap,
                             ),
                     )
                     lastPastedHashCode = clipboardContent.hashCode()
@@ -143,12 +212,81 @@ class JetpackComposeImpl(
                     ec = pageName,
                     v = "S~C~~${elementState.length}",
                     hv = elementState.getSHA256withSalt().take(8),
-                    attrs = listOf(
-                       sdkMap
-                    )
+                    attrs =
+                        listOf(
+                            sdkMap,
+                        ),
                 )
                 previousText = elementState
             }
+        }
+    }
+
+    /**
+     * Added as a secondary method in case the [trackElement] method is not available to use
+     */
+    override fun trackElementOnChange(
+        oldState: String,
+        newState: String,
+        elementName: String,
+        pageName: String,
+    ) {
+        if (!neuroID.isStopped() && newState != oldState) {
+            val clipboard = neuroID.getClipboardManagerInstance()
+            val clipData = clipboard?.primaryClip
+
+            var pastedText = ""
+            if (clipData != null && clipData.itemCount > 0) {
+                try {
+                    pastedText = clipData.getItemAt(0).text.toString()
+                } catch (e: Exception) {
+                    e.message?.let {
+                        logger.e("Activity", it)
+                    }
+                }
+            }
+
+            val changeText = neuroID.nidComposeTextWatcher.getTextChange(oldState, newState)
+            val clipboardContent = pastedText
+            // check for paste, send paste event if so
+            if (neuroID.nidComposeTextWatcher.isPaste(changeText, clipboardContent, oldState.hashCode())) {
+                neuroID.captureEvent(
+                    type = PASTE,
+                    tg =
+                        hashMapOf(
+                            "attr" to getAttrJson(newState),
+                            "et" to "text",
+                        ),
+                    tgs = elementName,
+                    ec = pageName,
+                    v = "S~C~~${clipboardContent.length}",
+                    hv = clipboardContent.getSHA256withSalt().take(8),
+                    attrs =
+                        listOf(
+                            mapOf(
+                                "clipboardText" to "S~C~~${clipboardContent.length}",
+                            ),
+                            sdkMap,
+                        ),
+                )
+            }
+            neuroID.captureEvent(
+                type = INPUT,
+                tg =
+                    hashMapOf(
+                        "attr" to getAttrJson(newState),
+                        "etn" to INPUT,
+                        "et" to "text",
+                    ),
+                tgs = elementName,
+                ec = pageName,
+                v = "S~C~~${newState.length}",
+                hv = newState.getSHA256withSalt().take(8),
+                attrs =
+                    listOf(
+                        sdkMap,
+                    ),
+            )
         }
     }
 }
