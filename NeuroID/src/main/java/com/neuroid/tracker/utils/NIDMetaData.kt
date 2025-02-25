2e965e9c67ce0a75be9599d1de777ffc84c7c326
kurt ishisaka <kurt.ishisaka@experian.com>
2025-02-25 13:35:59 -0800

    ENG-9331 remove last update time

--- NeuroID/src/main/java/com/neuroid/tracker/utils/NIDMetaData.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/utils/NIDMetaData.kt
@@ -30,7 +30,6 @@ class NIDMetaData(context: Context) {
     private val isSimulator: Boolean
     private val gpsCoordinates: NIDLocation = NIDLocation(-1.0, -1.0, LOCATION_UNKNOWN)
     private val lastInstallTime = context.packageManager?.getPackageInfo(context.packageName, 0)?.firstInstallTime ?: -1
-    private val lastUpdateTime = context.packageManager?.getPackageInfo(context.packageName, 0)?.lastUpdateTime ?: -1
 
     init {
         displayResolution = getScreenResolution(context)
@@ -121,7 +120,6 @@ class NIDMetaData(context: Context) {
         jsonObject.put("isSimulator", isSimulator)
         jsonObject.put("gpsCoordinates", gpsCoordinates.toJson())
         jsonObject.put("lastInstallTime", lastInstallTime)
-        jsonObject.put("lastUpdateTime", lastUpdateTime)
         return jsonObject
     }
 
