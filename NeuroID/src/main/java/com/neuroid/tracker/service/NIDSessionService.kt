a471b996edd4d213fa38fda0835a443174f458e1
Collin Dunphy <collin.dunphy@neuro-id.com>
2026-08-04 15:02:11 -0600

    [ENG-12000] Fix missing sdk version in early packets

--- NeuroID/src/main/java/com/neuroid/tracker/service/NIDSessionService.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/service/NIDSessionService.kt
@@ -13,6 +13,7 @@ import com.neuroid.tracker.models.SessionStartResult
 import com.neuroid.tracker.storage.NIDSharedPrefsDefaults
 import com.neuroid.tracker.utils.NIDLogWrapper
 import com.neuroid.tracker.utils.NIDSingletonIDs
+import com.neuroid.tracker.utils.NIDVersion
 import com.neuroid.tracker.utils.generateUniqueHexID
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
@@ -411,7 +412,7 @@ internal class NIDSessionService(
                 dnt = false,
                 url = "",
                 ns = "nid",
-                jsv = NeuroID.getInstance()?.getSDKVersion(),
+                jsv = NIDVersion.getSDKVersion(),
                 sw = sharedPreferenceDefaults.getDisplayWidth().toFloat(),
                 sh = sharedPreferenceDefaults.getDisplayHeight().toFloat(),
                 metadata = neuroID.metaData,
