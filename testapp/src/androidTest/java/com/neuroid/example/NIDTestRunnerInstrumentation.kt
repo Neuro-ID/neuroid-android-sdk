1c5a69c1119ea3f1014ac2da9e0fc6fe79ea96db
Kurt Bruck <Kurt.Bruck@experian.com>
2026-08-07 09:43:03 -0400

    setting up TestRunner extensions for more control and visibility during integration test automations

---
+++ testapp/src/androidTest/java/com/neuroid/example/NIDTestRunnerInstrumentation.kt
@@ -0,0 +1,60 @@
+package com.neuroid.example
+import android.os.Bundle
+import androidx.test.runner.AndroidJUnitRunner
+import com.google.gson.Gson
+import com.fingerprintjs.android.fpjs_pro.FingerprintJS
+import com.fingerprintjs.android.fpjs_pro.FingerprintJSProResponse
+import com.neuroid.tracker.NeuroID
+import io.mockk.every
+import io.mockk.mockk
+import com.fingerprintjs.android.fpjs_pro.Error
+
+class NIDTestRunnerInstrumentation  : AndroidJUnitRunner() {
+    private val gson = Gson()
+
+    private fun getMockedFPJSClient(
+        successResponse: String?,
+        errorResponse: String?,
+        sealedResult: String?
+    ): FingerprintJS {
+        val mockedFPJSClient = mockk<FingerprintJS>()
+        every { mockedFPJSClient.getVisitorId(tags = ofType<Map<String, Any>>(), listener = any(), errorListener = any()) }.answers {
+            if (successResponse != null) {
+                val successListener = args[1] as (FingerprintJSProResponse) -> Unit
+                val mockSuccessResponse = mockk<FingerprintJSProResponse>()
+                every {mockSuccessResponse.sealedResult} returns sealedResult
+                every {mockSuccessResponse.requestId} returns successResponse
+                successListener(mockSuccessResponse)
+            }
+            if (errorResponse != null) {
+                val errorListener = args[2] as (Error) -> Unit
+                val mockSuccessResponse = mockk<Error>()
+                every { mockSuccessResponse.description } returns errorResponse
+                errorListener(mockSuccessResponse)
+            }
+        }
+
+        return mockedFPJSClient
+    }
+    override fun onCreate(arguments: Bundle) {
+        NIDTestRunner.clearRecorders()
+        NeuroID.testFpjsClient = getMockedFPJSClient("test-visitor-id", null, null)
+        NeuroID.testOutboundPayloadObserver = { payload ->
+            runCatching {
+                gson.fromJson(payload, EventModel::class.java)
+            }.getOrNull()?.let { eventModel ->
+                MockServerHolder.attemptedRecorder.addEvent(eventModel)
+            }
+        }
+        val thread = Thread { NIDTestRunner.start() }
+        thread.start()
+        thread.join()
+        super.onCreate(arguments)
+    }
+
+    override fun finish(resultCode: Int, results: Bundle) {
+        NeuroID.clearTestObservers()
+        NIDTestRunner.shutdown()
+        super.finish(resultCode, results)
+    }
+}
