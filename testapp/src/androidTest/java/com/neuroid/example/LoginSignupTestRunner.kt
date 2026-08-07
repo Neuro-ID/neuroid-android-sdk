441dce5102358a94ab9bd6656644661bc568fdd7
Kurt Bruck <Kurt.Bruck@experian.com>
2026-08-07 11:01:55 -0400

    removed unnecessary perms

--- testapp/src/androidTest/java/com/neuroid/example/LoginSignupTestRunner.kt
+++ testapp/src/androidTest/java/com/neuroid/example/LoginSignupTestRunner.kt
@@ -42,9 +42,7 @@ class LoginSignupTestRunner {
     // Grant the required permissions for the test
     @get:Rule
     val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.READ_PHONE_STATE
+        Manifest.permission.ACCESS_FINE_LOCATION
     )
 
     @get:Rule
