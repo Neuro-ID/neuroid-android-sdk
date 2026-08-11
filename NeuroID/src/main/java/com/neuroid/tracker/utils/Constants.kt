877d8ccbd85e5499b06524932a3f447f5488d37f
Collin Dunphy <collin.dunphy@neuro-id.com>
2026-08-11 09:50:19 -0600

    Remove extra constants

--- NeuroID/src/main/java/com/neuroid/tracker/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.neuroid.tracker.utils
-
-@Suppress("ktlint:standard:enum-entry-name-case")
-enum class Constants(
-    val displayName: String,
-) {
-    devEndpoint("https://receiver.neuro-dev.com/"),
-    devScriptsEndpoint("https://scripts.neuro-dev.com/"),
-}
