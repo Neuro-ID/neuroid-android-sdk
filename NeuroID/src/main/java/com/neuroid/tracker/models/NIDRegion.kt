d3510228f359a43cea7110b753bc6a5e83df1112
kurt ishisaka <kurt.ishisaka@experian.com>
2026-09-11 12:05:56 -0700

    ENG-12224 Add usWest2 and usEast2 setting to region configuration

--- NeuroID/src/main/java/com/neuroid/tracker/models/NIDRegion.kt
+++ NeuroID/src/main/java/com/neuroid/tracker/models/NIDRegion.kt
@@ -13,6 +13,18 @@ enum class NIDRegion(
         productionEndpoint = "https://receiver.neuroid.cloud/",
         productionScriptsEndpoint = "https://scripts.neuro-id.com/",
     ),
+    usWest2(
+        fpjsProdDomain = "https://advanced.neuro-id.com",
+        fpjsPrimaryDomain = "https://dn.neuroid.cloud/iynlfqcb0t",
+        productionEndpoint = "https://edge.neuroid.cloud/usw2/",
+        productionScriptsEndpoint = "https://scripts.neuro-id.com/"
+    ),
+    usEast2(
+        fpjsProdDomain = "https://advanced.neuro-id.com",
+        fpjsPrimaryDomain = "https://dn.neuroid.cloud/iynlfqcb0t",
+        productionEndpoint = "https://edge.neuroid.cloud/use2/",
+        productionScriptsEndpoint = "https://scripts.neuro-id.com/"
+    ),
     TEST(
         fpjsProdDomain = "http://127.0.0.1:8000",
         fpjsPrimaryDomain = "http://127.0.0.1:8000",
